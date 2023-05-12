4949aab23ce03b94e4cbd11107bea8849f2ccd5b
mohammad rezaii <rezaiimohammad003@gmail.com>
2023-05-12 17:41:29 +0330

    add changes of the index.html and script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const myName = "Mohammad";
 const myFamily = "Rezaii";
 
+// this function logs the user's full name
 function logMyName(name, family) {
 	console.log(`my name is ${name} ${family}`);
 }
